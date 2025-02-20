b111d8ed4d7039f69dee1e43ecbd3245b4f90890
KNToad <tendmitriy02052016@gmail.com>
2025-02-20 10:18:41 +0300

    init commit

---
+++ src/test/java/com/skypro/employees_mockito_hw/EmployeesMockitoHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.employees_mockito_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesMockitoHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
